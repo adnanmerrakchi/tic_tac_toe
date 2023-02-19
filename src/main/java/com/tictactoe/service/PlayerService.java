c925818a447ea882df19b5ad9d8cc2ce50154fda
adnanmerrakchi <adnanmerrakchi@gmail.com>
2023-02-19 15:11:07 +0100

    make findWinnerMark tests green :D.

--- src/main/java/com/tictactoe/service/PlayerService.java
+++ src/main/java/com/tictactoe/service/PlayerService.java
@@ -7,7 +7,67 @@ import org.springframework.stereotype.Service;
 @Service
 public class PlayerService {
 
+    private StateBoardService stateBoardService;
+
+    public PlayerService(StateBoardService stateBoardService) {
+        this.stateBoardService = stateBoardService;
+    }
+
     public Mark findWinner(Board board){
+        //  *   *   *
+        //  *   *   *
+        //  *   *   * ???
+        if(!stateBoardService.isBoardDone(board)){
+            return null;
+        }   else
+        //  *   *   *
+        //  -   -   -
+        //  -   -   -
+        if(board.getCell1().equals(board.getCell2()) && board.getCell1().equals(board.getCell3())){
+            return board.getCell1();
+        }   else
+        //  -   -   -
+        //  *   *   *
+        //  -   -   -
+        if(board.getCell4().equals(board.getCell5()) && board.getCell4().equals(board.getCell6())){
+            return board.getCell4();
+        }   else
+        //  -   -   -
+        //  -   -   -
+        //  *   *   *
+        if(board.getCell7().equals(board.getCell8()) && board.getCell7().equals(board.getCell9())){
+            return board.getCell7();
+        }   else
+        //  *   -   -
+        //  *   -   -
+        //  *   -   -
+        if(board.getCell1().equals(board.getCell4()) && board.getCell1().equals(board.getCell7())){
+            return board.getCell1();
+        }   else
+        //  *   -   -
+        //  -   *   -
+        //  -   -   *
+        if(board.getCell1().equals(board.getCell5()) && board.getCell1().equals(board.getCell9())){
+            return board.getCell1();
+        }   else
+        //  -   *   -
+        //  -   *   -
+        //  -   *   -
+        if(board.getCell2().equals(board.getCell5()) && board.getCell2().equals(board.getCell8())){
+            return board.getCell2();
+        }   else
+        //  -   -   *
+        //  -   -   *
+        //  -   -   *
+        if(board.getCell3().equals(board.getCell6()) && board.getCell3().equals(board.getCell9())){
+            return board.getCell3();
+        }   else
+        //  -   -   *
+        //  -   *   -
+        //  *   -   -
+        if(board.getCell3().equals(board.getCell5()) && board.getCell3().equals(board.getCell7())){
+            return board.getCell3();
+        }
         return null;
     }
 
--- src/test/java/com/tictactoe/service/PlayerServiceTest.java
+++ src/test/java/com/tictactoe/service/PlayerServiceTest.java
@@ -2,28 +2,46 @@ package com.tictactoe.service;
 
 import com.tictactoe.model.Board;
 import com.tictactoe.model.Mark;
+import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class PlayerServiceTest {
+
+    @InjectMocks
+    PlayerService playerService;
+
+    @Mock
+    StateBoardService stateBoardService;
+
+    @BeforeEach
+    void init(){
+    }
 
 
     @Test
     public void findWinnerMark_xIsWinner(){
         Board board = new Board(Mark.X, Mark.X, Mark.X,
                                 Mark.X, Mark.O, Mark.X,
-                                null,   Mark.O, null);
-        PlayerService playerService = new PlayerService();
+                                Mark.O, Mark.O, Mark.O);
+        Mockito.when(stateBoardService.isBoardDone(Mockito.any(Board.class))).thenReturn(true);
         Assertions.assertEquals(playerService.findWinner(board), Mark.X);
 
     }
 
     @Test
     public void findWinnerMark_oIsWinner(){
-        Board board = new Board(Mark.O, null,   Mark.X,
+        Board board = new Board(Mark.O, Mark.O,   Mark.X,
                                 Mark.X, Mark.O, Mark.X,
-                                null,   Mark.O, Mark.O);
-        PlayerService playerService = new PlayerService();
+                                Mark.X,   Mark.O, Mark.O);
+        Mockito.when(stateBoardService.isBoardDone(Mockito.any(Board.class))).thenReturn(true);
         Assertions.assertEquals(playerService.findWinner(board), Mark.O);
     }
 
@@ -32,7 +50,7 @@ public class PlayerServiceTest {
         Board board = new Board(Mark.O, null,   Mark.X,
                                 Mark.X, Mark.O, Mark.X,
                                 null,   Mark.O, Mark.O);
-        PlayerService playerService = new PlayerService();
+        Mockito.when(stateBoardService.isBoardDone(Mockito.any(Board.class))).thenReturn(false);
         Assertions.assertEquals(playerService.findWinner(board), null);
     }
 }
